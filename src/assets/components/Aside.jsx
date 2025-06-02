d7a0b769a56f75997975b0e8afc1cb8bcefecd0d
camilo <camicastrillon2314@gmail.com>
2025-06-01 22:13:59 -0500

    se termina el proyecto, listo para siguientes imagenes

--- src/assets/components/Aside.jsx
+++ src/assets/components/Aside.jsx
@@ -1,39 +1,25 @@
-import React from 'react'
 import './../Styles/Inicio.css'
 import { Link } from 'react-router-dom'
-import WPT from "../icon/icono-WT.png"
-import CR from "../icon/icono-correo.png"
-import TF from "../icon/icono-telefono.png"
 import "../Styles/Aside.css"
 import Foto from "../Trabajos/IMG-20250523-WA0009.jpg"
 
 const Aside = () => {
-const Contactos =[
-    {name:WPT,Tcontacto:"https://wa.me/+573226683797",valor:"Whatsapp"},
-    {name:CR,Tcontacto:"https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=elkindario525@gmail.com",valor:"Correo electronico"},
-    {name:TF,Tcontacto:"+57 3226683797",valor:"+57 3226683797"}
-  ]
-  const ContatosCard = ({name,Tcontacto,valor})=>(
-    <div>
-       <img className='img-contacto' src={name} alt="" />
-       <Link className='contacto'to={Tcontacto} target='_black' >{valor}</Link> 
-    </div>
-  )
+
   return (
-    <aside className='conternedor-contacto' >
-      <img className='conternedor-contacto-img' src={Foto} alt="wt" />
-      <p className='titulo-contacto'>Contacto</p>
-      <div className='contactos-card'>
-      {Contactos.map((Contacto)=>(
-        <ContatosCard
-        key={Contacto.img}
-        name={Contacto.name} 
-        Tcontacto={Contacto.Tcontacto}
-        valor={Contacto.valor}
-        />
-      ))}
+    <>
+      <div className='conternedor-contacto' >
+        <img className='contenedor-contacto-img' src={Foto} alt="wt" />
+        <p className='titulo-contacto'>Contacto</p>
+        <div className='contactos-card'>
+          <Link className='contacto' to='https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=elkindario525@gmail.com' target='_black' >Gmail
+          </Link>
+          <Link className='contacto' to='+57 3226683797' target='_black' >3226683797
+          </Link>
+          <Link className='contacto' to="https://wa.me/+573226683797" target='_black' >Whatsapp
+          </Link>
+        </div>
       </div>
-      </aside>
+    </>
   )
 }
 
--- src/assets/pages/Inicio.jsx
+++ src/assets/pages/Inicio.jsx
@@ -10,6 +10,7 @@ import Foto8 from "./../Trabajos/IMG-20250523-WA0005.jpg"
 import Foto9 from "./../Trabajos/IMG-20250523-WA0006.jpg"
 import Foto10 from "./../Trabajos/IMG-20250523-WA0007.jpg"
 import Foto11 from "./../Trabajos/IMG-20250523-WA0008.jpg"
+import Foto12 from "./../Trabajos/Captura de pantalla 2025-05-27 224102.png"
 import video1 from "./../Trabajos/Vídeo sin título Hecho con Clipchamp.mp4"
 import video2 from "./../Trabajos/Vídeo sin título2 Hecho con Clipchamp.mp4"
 import video3 from "./../Trabajos/Vídeo sin título3 Hecho con Clipchamp.mp4"
@@ -24,29 +25,31 @@ const Inicio = () => {
   ]
   const trabajos_imagen = [
 
-    {foto:Foto1},
-    {foto:Foto2},
-    {foto:Foto3},
-    {foto:Foto4},
-    {foto:Foto5},
-    {foto:Foto6},
-    {foto:Foto7},
-    {foto:Foto8},
-    {foto:Foto9},
-    {foto:Foto10},
-    {foto:Foto11}
+    {foto:Foto1,id:1},
+    {foto:Foto2,id:2},
+    {foto:Foto3,id:3},
+    {foto:Foto4,id:4},
+    {foto:Foto5,id:5},
+    {foto:Foto6,id:6},
+    {foto:Foto7,id:7},
+    {foto:Foto8,id:8},
+    {foto:Foto9,id:9},
+    {foto:Foto10,id:10},
+    {foto:Foto11,id:11},
+    {foto:Foto12,id:12}
   ]
 
   const Videos = [
-    {video:video1},
-    {video:video2},
-    {video:video3},
-    {video:video4},
-    {video:video5}
+    {video:video1,id:1},
+    {video:video2,id:2},
+    {video:video3,id:3},
+    {video:video4,id:4},
+    {video:video5,id:5}
 ]
   return (
     <>
-      <Aside></Aside>
+      <section className='fondo'>
+      <section className='aside'><Aside></Aside></section>
       <section className='contenedor-descripcion'>
       <h1 className='introduccion' >Hola, soy Elkin Dario Castrillón,<br/>con más de 30 años de experiencia en la construcción y remodelación de edificaciones</h1>
       <p className='descripcion' >A lo largo de mi trayectoria, he trabajado en proyectos residenciales, comerciales e industriales, especializándome en estructuras de mampostería, acabados y restauraciones. Mi enfoque está en la precisión, el uso eficiente de materiales y el cumplimiento de los más altos estándares de seguridad y calidad. Me apasiona el oficio y siempre busco soluciones prácticas y duraderas para cada proyecto, garantizando satisfacción y confianza a mis clientes y empleadores además de un excelente uso del orden y el cuidado de mi area de trabajo, caracterizandome por la pulcritud y eficiencia.</p>
@@ -60,7 +63,7 @@ const Inicio = () => {
         <h2>Estos son algunos de mis trabajos</h2>
         <section className='contenedor-trabajos-imagen'>
         {trabajos_imagen.map((foto)=>(
-          <img className='trabajo-foto' src={foto.foto} alt="foto del trabajo realizado" />
+          <img className='trabajo-foto' key={foto.id} src={foto.foto} alt="foto del trabajo realizado" />
         ))}
         </section>
         </section>
@@ -68,12 +71,11 @@ const Inicio = () => {
           <h2>Con estos videos podrás apreciar mejor el trabajo</h2>
           <section className="contenedor-trabajos-imagen">
           {Videos.map((video)=>(
-            <video className='trabajo-foto' src={video.video} loop controls ></video>
+            <video className='trabajo-foto' key={video.id} src={video.video} loop controls ></video>
           ))}
           </section>
+      </section> 
       </section>
-
-      
     
     </>
   )
